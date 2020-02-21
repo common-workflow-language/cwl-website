1bc5c0896d3bfdb93ae2a7aab8fd57329771f68a
Peter Amstutz <peter.amstutz@curii.com>
2020-02-21 14:26:07 -0500

    Add CWL 1.2 draft spec

--- website.sh
+++ website.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-rm -Rf draft-3 v1.0 v1.1.0-dev1 v1.1.0 v1.1 temp
+rm -Rf draft-3 v1.0 v1.1.0-dev1 v1.1.0 v1.1 v1.2 temp
 
 git clone https://github.com/common-workflow-language/common-workflow-language.git \
 	temp
@@ -11,5 +11,7 @@ mv temp/v1.0 ./
 mv temp/draft-3 ./
 git clone https://github.com/common-workflow-language/cwl-v1.1.git \
 	v1.1
+git clone https://github.com/common-workflow-language/cwl-v1.2.git \
+	v1.2
 
 cwltool $@ --cache cache --outdir common-workflow-language.github.io site/cwlsite.cwl site/cwlsite-job.yaml
